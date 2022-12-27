9f9c9744624d7c369c1254856ba8d2d3e378c99b
PauloGabrieel <gabrieeel.c@gmail.com>
2022-12-27 15:11:29 -0300

    chore: install jwt

--- src/middlewares/tokenValidation.ts
+++ src/middlewares/tokenValidation.ts
@@ -1,10 +1,8 @@
-import { db } from "../dbStrategy/postgres.js"
 
 export default async function tokenValidation(req, res, next){
     const {authorization} = req.headers;
     const token = authorization?.replace("Bearer ", "");
     
-    const session = await db.collection("sessions").findOne({token});
     
     if(!session){
         return res.status(404).send("token não existe!");
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import { createUser, loginUser } from "../controllers/authControllers.js";
-import signUpValidatin from "../middlewares/signUpValidation.js";
-import signinValidation from "../middlewares/signinValidation.js";
 const router = Router();
 
-router.post("/signup", signUpValidatin, createUser);
-router.post("/signin", signinValidation, loginUser);
+router.post("/signup", createUser);
+router.post("/signin", loginUser);
 
 export default router;
