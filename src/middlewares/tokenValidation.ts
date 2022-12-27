624108bf1947bc0e2e948fb28ab04964c927938b
PauloGabrieel <gabrieeel.c@gmail.com>
2022-12-27 15:44:43 -0300

    refact: use jwt for validation

--- src/middlewares/tokenValidation.ts
+++ src/middlewares/tokenValidation.ts
@@ -1,12 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import prisma from "../dbStrategy/postgres";
+import * as jwt from "jsonwebtoken";
 
-export default async function tokenValidation(req, res, next){
-    const {authorization} = req.headers;
-    const token = authorization?.replace("Bearer ", "");
-    
-    
-    if(!session){
-        return res.status(404).send("token não existe!");
-    };
-    res.locals.session = session;
-    next();
+export default async function tokenValidation(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const {authorization} = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  
+  if(!token) return res.status(403).send("You must be signed in to continue");
+  
+  try {
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET) as JWTPayload;
+
+    const session = await prisma.session.findFirst({
+      where: {
+       token,
+      }
+    });
+
+    if(!session) return res.status(403).send("You must be signed in to continue");
+    req.userId = userId
+    return next();
+  } catch (error) {
+    return res.status(403).send("You must be signed in to continue");
+  }
 };
+
+
+
+export type AuthenticatedRequest = Request & JWTPayload;
+
+type JWTPayload = {
+    userId: number;
+  };
